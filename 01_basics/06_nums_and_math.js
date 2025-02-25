bfb7e65e85ce7629b5df6c42344f086944a51e51
yuvrajsk271 <yuvrajsk271@gmail.com>
2025-02-25 10:40:30 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -5,7 +5,8 @@ const balance = new Number(100)
 // console.log(balance);
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(3)); 
+//to fixed will give me 100.000
 
 const otherNumber = 123.8966
 
